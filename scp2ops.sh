1a7ab45a857652e86b9b4415036df7980abb28ab
Yudai Aratsu <yudarats@yahoo-corp.jp>
2020-10-14 10:43:14 +0900

    update

--- scp2ops.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp mac/{.vimrc,.zshrc,.tmux.conf} yj-ops01:~/
-scp mac/{.vimrc,.zshrc,.tmux.conf} yj-ops02:~/
--- scp2yudarats.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp mac/{.vimrc,.zshrc,.tmux.conf} yj-yudarats01:~/
-ssh yj-yudarats01 "rm -rf .vim .zplug .zcompdump"
